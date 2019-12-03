bdd7adab6a5528e2f77f14248973997b4bcab7b1
KDS <ds.krav@gmail.com>
2019-12-03 19:55:01 +0800

    add header and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  BOOTSTRAP_ALERTS = { 'notice' => 'alert-success', 'alert' => 'alert-danger' }.freeze
+  DEFAULT_BOOTSTRAP_ALERT = 'alert-primary'.freeze
+
+  def flash_message(type, msg)
+    alert_type = BOOTSTRAP_ALERTS[type] || DEFAULT_BOOTSTRAP_ALERT
+    content_tag :div, sanitize(msg, attributes: %w[href target]),
+                class: "alert #{alert_type}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'movings#index'
+
   resources :shipments
   resources :users
   resources :movings
